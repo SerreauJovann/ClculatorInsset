d8218afdf6ae075e195922a6c616b5769f3c480d
Serreau Jovann <jovanndeveloppeur@gmail.com>
2018-11-08 20:26:54 +0100

    conversion entier en romain

--- src/main/java/org/insset/server/RomanConverterServiceImpl.java
+++ src/main/java/org/insset/server/RomanConverterServiceImpl.java
@@ -30,25 +30,78 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements
 
     @Override
     public String convertArabeToRoman(Integer nbr) throws IllegalArgumentException {
-       int minIn = 1;
-       int maxIn = 2000;
+        int minIn = 1;
+        int maxIn = 2000;
        
-       if(nbr !=null)
-       {
-           if(nbr >=minIn && nbr <=maxIn)
-           {
-               
-           }
-           else
-           {
-               new IllegalArgumentException("valeur incorrect");
-           }
-       }
-       else
-       {
+        if(nbr !=null)
+        {
+            if(nbr >=minIn && nbr <=maxIn)
+            {
+                String result = "";
+                 while (nbr >= 1000) {
+                     result += "M";
+                     nbr -= 1000;
+                 }
+                while (nbr >= 900) {
+                    result += "CM";
+                    nbr -= 900;
+                }
+                while (nbr >= 500) {
+                    result += "D";
+                    nbr -= 500;
+                }
+                while (nbr >= 400) {
+                    result += "CD";
+                    nbr -= 400;
+                }
+                while (nbr >= 100) {
+                    result += "C";
+                    nbr -= 100;
+                }
+                while (nbr >= 90) {
+                    result += "XC";
+                    nbr -= 90;
+                }
+                while (nbr >= 50) {
+                    result += "L";
+                    nbr -= 50;
+                }
+                while (nbr >= 40) {
+                    result += "XL";
+                    nbr -= 40;
+                }
+                while (nbr >= 10) {
+                    result += "X";
+                    nbr -= 10;
+                }
+                while (nbr >= 9) {
+                    result += "IX";
+                    nbr -= 9;
+                }
+                while (nbr >= 5) {
+                    result += "V";
+                    nbr -= 5;
+                }
+                while (nbr >= 4) {
+                    result += "IV";
+                    nbr -= 4;
+                }
+                while (nbr >= 1) {
+                    result += "I";
+                    nbr -= 1;
+                }    
+                return result;
+            }
+            else
+            {
+                new IllegalArgumentException("valeur incorrect");
+            }
+        }
+        else
+        {
             new IllegalArgumentException("valeur incorrect");
-       }
-       return "ok";
+        }
+        return "";
     }
 
 }
